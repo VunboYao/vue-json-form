d3930e2a0be9331eed0b27d4359e6c38a092dd95
Vunbo MacMini <vunbo@outlook.com>
2022-03-16 22:43:26 +0800

    style: eslint优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     node: true,
   },
   extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    'plugin:prettier/recommended',
-    './.eslintrc-auto-import.json'
+    // 'plugin:prettier/recommended',
+    './.eslintrc-auto-import.json',
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,6 +18,22 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'off',
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 1
+      },
+      "multiline": {
+        "max": 1
+      }
+    }],
+    // 单行属性时，不换行
+    "vue/first-attribute-linebreak": ['error', {
+      "singleline": "beside"
+    }],
+    "semi": ["error", "never"],
+    "arrow-parens": ["error", "always"], // 箭头函数必须括号
+    "vue/comment-directive": "off", // 去掉html的检测
   },
   overrides: [
     {
