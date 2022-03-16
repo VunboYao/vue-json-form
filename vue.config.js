9623636b51b2e04f4a61268c797f9ef8d4840641
VunboYao <vunbo@outlook.com>
2022-03-16 09:16:44 +0800

    feat(vue.config.js): 自动导入vue配置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
-const autoImport = require('unplugin-auto-import/webpack')
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
-      autoImport({
+      require('unplugin-auto-import/webpack')({
         include: [/\.[tj]sx?$/, /\.vue$/],
-        import: ['vue'],
+        imports: ['vue'],
       }),
     ],
   },
